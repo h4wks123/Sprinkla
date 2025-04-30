f153733650d11dc843c7671a3dff9e1f841c9c04
h4wks123 <ivanne_bayer@LAPTOP-31P56QLH.>
2025-04-30 22:34:44 +0800

    fix: changed form implementation by removing useRef

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -10,18 +10,21 @@ import loginUser from "@/libs/database/queries/users/loginUsers";
 
 const Login = () => {
   const router = useRouter();
-  let emailInput = useRef<HTMLInputElement>(null);
-  let passwordInput = useRef<HTMLInputElement>(null);
 
-  async function submitLoginForm() {
+  async function submitLoginForm(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    const emailInput = formData.get("email") as string;
+    const passwordInput = formData.get("password") as string;
+
+    e.currentTarget.reset();
+
     if (!emailInput || !passwordInput) {
       throw new Error("Login inputs cannot be empty!");
     } else {
-      let email = emailInput.current!.value;
-      let password = passwordInput.current!.value;
-
-      emailInput.current!.value = "";
-      passwordInput.current!.value = "";
+      let email = emailInput;
+      let password = passwordInput;
 
       const loginStatus = await loginUser(email, password);
 
@@ -33,21 +36,32 @@ const Login = () => {
 
   return (
     <section className="w-full h-[100dvh] flex justify-center align-middle">
-      <form className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[90%] max-w-[540px] rounded-md flex flex-col items-center gap-6 pb-12 bg-secondary">
+      <form
+        onSubmit={submitLoginForm}
+        className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[90%] max-w-[540px] rounded-md flex flex-col items-center gap-6 pb-12 bg-secondary"
+      >
         <div className="relative w-full h-64">
           <Image src="/login_modal_image.png" alt="login modal image" fill />
         </div>
         <h1 className="mt-6">SPRINKLA</h1>
         <div className="w-[80%]">
-          <h6>Email</h6>
-          <input className="bg-white w-full text-black" ref={emailInput} />
+          <label>Email</label>
+          <input
+            type="text"
+            name="email"
+            className="bg-white w-full text-black"
+          />
         </div>
         <div className="w-[80%]">
           <h6>Password</h6>
-          <input className="bg-white w-full text-black" ref={passwordInput} />
+          <input
+            type="text"
+            name="password"
+            className="bg-white w-full text-black"
+          />
         </div>
         <button
-          onClick={submitLoginForm}
+          type="submit"
           className="cursor-pointer w-44 h-8 rounded-md bg-quinary"
         >
           Login
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useRef } from "react";
+import React, { FormEvent, useRef } from "react";
 
 import Link from "next/link";
 import Image from "next/image";
@@ -8,51 +8,61 @@ import Image from "next/image";
 import registerUser from "@/libs/database/queries/users/createUsers";
 
 const Register = () => {
-  let emailInput = useRef<HTMLInputElement>(null);
-  let passwordInput = useRef<HTMLInputElement>(null);
-  let contactNumberInput = useRef<HTMLInputElement>(null);
+  function submitRegisterForm(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
 
-  function submitRegisterForm() {
+    let formData = new FormData(e.currentTarget);
+    let emailInput = formData.get("email") as string;
+    let passwordInput = formData.get("password") as string;
+    let contactNumberInput = Number(formData.get("contactNumber"));
+
+    e.currentTarget.reset();
+
     if (!emailInput || !passwordInput || !contactNumberInput) {
       throw new Error("Inputs cannot be empty");
-    } else {
-      registerUser(
-        emailInput.current!.value,
-        passwordInput.current!.value,
-        Number(contactNumberInput.current!.value)
-      );
-    }
-
-    emailInput.current!.value = "";
-    passwordInput.current!.value = "";
-    contactNumberInput.current!.value = "";
+    } 
+    // else {
+    //   registerUser(emailInput, passwordInput, contactNumberInput);
+    // }
   }
 
   return (
     <section className="w-full h-[100dvh] flex justify-center align-middle">
-      <article className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[90%] max-w-[540px] rounded-md flex flex-col items-center gap-6 pb-12 bg-secondary">
+      <form
+        onClick={submitRegisterForm}
+        className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[90%] max-w-[540px] rounded-md flex flex-col items-center gap-6 pb-12 bg-secondary"
+      >
         <div className="relative w-full h-64">
           <Image src="/login_modal_image.png" alt="login modal image" fill />
         </div>
         <h1 className="mt-6">SPRINKLA</h1>
         <div className="w-[80%]">
           <h6>Email</h6>
-          <input className="w-full bg-white text-black" ref={emailInput} />
+          <input
+            type="text"
+            name="email"
+            className="w-full bg-white text-black"
+          />
         </div>
         <div className="w-[80%]">
           <h6>Password</h6>
-          <input className="w-full bg-white text-black" ref={passwordInput} />
+          <input
+            type="text"
+            name="password"
+            className="w-full bg-white text-black"
+          />
         </div>
         <div className="w-[80%]">
           <h6>Contact Number</h6>
           <input
+            type="text"
+            name="contactNumber"
             className="w-full bg-white text-black"
-            ref={contactNumberInput}
           />
         </div>
         <button
+          type="submit"
           className="cursor-pointer w-44 h-8 rounded-md bg-quinary"
-          onClick={submitRegisterForm}
         >
           Register
         </button>
@@ -62,7 +72,7 @@ const Register = () => {
             Sign In
           </Link>
         </h6>
-      </article>
+      </form>
     </section>
   );
 };
