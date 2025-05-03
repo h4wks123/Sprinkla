2b12e681bd08c9a96184aa2bc1b647f6026e0254
h4wks123 <ivanne_bayer@LAPTOP-31P56QLH.>
2025-05-03 09:57:03 +0800

    feat: partially finished login UI design

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -18,7 +18,84 @@ const Login = () => {
   );
 
   return (
-    <section className="w-full h-[100dvh] flex justify-center align-middle">
+    <section className="w-full h-[100dvh]">
+      <Image
+        src="bezier_curve_2.svg"
+        alt="bezier_curve_2"
+        width={0}
+        height={0}
+        style={{
+          position: "fixed",
+          width: "28%",
+          minWidth: "400px",
+          height: "auto%",
+          animationName: "login_bezier_curves",
+          animationDuration: "40s",
+          animationIterationCount: "infinite",
+          animationDirection: "alternate",
+          animationTimingFunction: "ease-in-out",
+        }}
+      />
+      <Image
+        src="bezier_curve_1.svg"
+        alt="bezier_curve_1"
+        width={0}
+        height={0}
+        style={{
+          position: "fixed",
+          bottom: 0,
+          right: 0,
+          width: "28%",
+          minWidth: "400px",
+          height: "auto%",
+          animationName: "login_bezier_curves",
+          animationDuration: "40s",
+          animationIterationCount: "infinite",
+          animationDirection: "alternate",
+          animationTimingFunction: "ease-in-out",
+        }}
+      />
+      <Image
+        src="/login_donut_1.png"
+        alt="login_donut_1"
+        width={425}
+        height={425}
+        quality={100}
+        style={{
+          position: "fixed",
+          top: -40,
+          right: -50,
+          animationName: "login_donut_1",
+          animationDuration: "40s",
+          animationIterationCount: "infinite",
+        }}
+      />
+      <Image
+        src="/login_donut_2.png"
+        alt="login_donut_1"
+        width={525}
+        height={525}
+        quality={100}
+        style={{
+          position: "fixed",
+          bottom: -20,
+          left: -30,
+          animationName: "login_donut_1",
+          animationDuration: "40s",
+          animationIterationCount: "infinite",
+        }}
+      />
+      <Image
+        src="/login_triangles.svg"
+        alt="login_triangles"
+        width={0}
+        height={0}
+        style={{
+          height: "100%",
+          width: "auto",
+          position: "fixed",
+        }}
+      />
       <form
         onSubmit={submitLoginForm}
         className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[90%] max-w-[540px] rounded-md flex flex-col items-center pb-12 bg-secondary"
@@ -26,7 +103,15 @@ const Login = () => {
         <div className="relative w-full h-64">
           <Image src="/login_modal_image.png" alt="login modal image" fill />
         </div>
-        <h1 className="mt-6">SPRINKLA</h1>
+        <Image
+          src="/sprinkla_logo.svg"
+          alt="spinkla_logo"
+          width={275}
+          height={275}
+          style={{
+            marginTop: "calc(var(--spacing) * 6)",
+          }}
+        />
         <div className="w-[80%] h-18 mt-6">
           <label>Email</label>
           <input
